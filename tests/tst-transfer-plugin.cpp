305d2fdfaadf84eeee9a05293e876d8e75bfe81f
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2015-09-08 14:08:31 -0300

    Updated test to check for custom state strings.

--- tests/tst-transfer-plugin.cpp
+++ tests/tst-transfer-plugin.cpp
@@ -23,14 +23,16 @@ private:
         typedef enum { ADDED, CHANGED, REMOVED } Type;
         Type type;
         QString profileId;
+        ButeoTransfer transfer;
 
-        Event(Type t, const QString &s)
-            : type(t), profileId(s)
+        Event(Type t, const QString &s, const ButeoTransfer &bt)
+            : type(t), profileId(s), transfer(bt)
         {}
 
         Event(const Event &other)
             : type(other.type),
-              profileId(other.profileId)
+              profileId(other.profileId),
+              transfer(other.transfer)
         {}
 
         bool operator==(const Event& that) const
@@ -46,11 +48,17 @@ private Q_SLOTS:
         QQueue<Event> events;
 
         plugin->get_model()->added().connect([&events, &plugin](const Transfer::Id& id){
-            events.append(Event(Event::ADDED, QString::fromStdString(id)));
+            ButeoTransfer bt(*static_cast<ButeoTransfer*>(plugin->get_model()->get(id).get()));
+            events.append(Event(Event::ADDED,
+                                QString::fromStdString(id),
+                                bt));
         });
 
         plugin->get_model()->changed().connect([&events, &plugin](const Transfer::Id& id){
-            events.append(Event(Event::CHANGED, QString::fromStdString(id)));
+            ButeoTransfer bt(*static_cast<ButeoTransfer*>(plugin->get_model()->get(id).get()));
+            events.append(Event(Event::CHANGED,
+                                QString::fromStdString(id),
+                                bt));
         });
 
         // start sync
@@ -62,26 +70,31 @@ private Q_SLOTS:
         Event e = events.takeFirst();
         QCOMPARE(e.type, Event::ADDED);
         QCOMPARE(e.profileId, QStringLiteral("profile-123"));
+        QCOMPARE(QString::fromStdString(e.transfer.custom_state), QStringLiteral(""));
 
         // changed(QUEUED)
         e = events.takeFirst();
         QCOMPARE(e.type, Event::CHANGED);
         QCOMPARE(e.profileId, QStringLiteral("profile-123"));
+        QCOMPARE(QString::fromStdString(e.transfer.custom_state), QStringLiteral(""));
 
         // changed(STARTED)
         e = events.takeFirst();
         QCOMPARE(e.type, Event::CHANGED);
         QCOMPARE(e.profileId, QStringLiteral("profile-123"));
+        QCOMPARE(QString::fromStdString(e.transfer.custom_state), QStringLiteral("Syncing"));
 
         // changed(RUNNING)
         e = events.takeFirst();
         QCOMPARE(e.type, Event::CHANGED);
         QCOMPARE(e.profileId, QStringLiteral("profile-123"));
+        QCOMPARE(QString::fromStdString(e.transfer.custom_state), QStringLiteral("Syncing"));
 
         // changed(FINISHED)
         e = events.takeFirst();
         QCOMPARE(e.type, Event::CHANGED);
         QCOMPARE(e.profileId, QStringLiteral("profile-123"));
+        QCOMPARE(QString::fromStdString(e.transfer.custom_state), QStringLiteral(""));
     }
 };
 
